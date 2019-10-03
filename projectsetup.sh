20da0f3550530e083094a49bc364b9be719a5d87
Romain Boudot <romain.boudot@epsi.fr>
2019-10-03 19:54:24 +0200

    fixing issues in installing process

--- projectsetup.sh
+++ projectsetup.sh
@@ -9,6 +9,12 @@ php artisan migrate --seed
 # php artisan config:cache
 # php artisan route:cache
 
+echo " | installing and building vue project (it might take few minutes)"
 cd /vagrant/client_vuejs
-npm install
-yarn build
+sudo npm install
+sudo npm install -g @vue/cli
+sudo npm install -g @vue/cli-service
+sudo npm install -g @vue/cli-plugin-babel
+sudo npm install -g @vue/cli-plugin-eslint
+sudo npm install -g vue-template-compiler
+sudo yarn build
